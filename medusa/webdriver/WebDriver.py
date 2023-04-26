f762fdc262404a0a681179272343d8699ce51f1d
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-04-26 14:31:23 -0400

    implemented code to exit program gracefully by killing the webdriver process and exiting the actual browser

--- medusa/webdriver/WebDriver.py
+++ medusa/webdriver/WebDriver.py
@@ -6,23 +6,25 @@ from time import sleep
 
 
 class WebDriver:
+  """
+  Interface to interact with chrome using the chromium webdriver.
+  """
   def __init__(self):
+    """
+    Initializes this WebDriver object.
+    """
     self.popen = self._init_driver()
     self.session_id = self._get_session_id()
 
   def _get_session_id(self):
     """
     Gets a valid session id for this webdriver session.
-    """
-    """
-    Summary or Description of the Function
-
-    Parameters:
-    argument1 (int): Description of arg1
 
     Returns:
-    int:Returning value
+    str:session id
 
+    Throws:
+    DriverInitializationError:error on request failing
     """
     _attempts = 0
 
@@ -43,6 +45,15 @@ class WebDriver:
         raise DriverInitializationError('Failed to get session id.')
 
   def _init_driver(self):
+    """
+    Initializes the driver process for this webdriver.
+
+    Returns:
+    subprocess.Popen:driver process
+
+    Throws:
+    DriverInitializationError:error on driver creation failing
+    """
     try:
       return subprocess.Popen([
         DRIVER_PATH, 
@@ -51,3 +62,24 @@ class WebDriver:
 
     except:
       raise DriverInitializationError('Failed to initialize chrome driver.')
+    
+  def _kill(self):
+    """
+    Kills the webdriver process.
+    """
+    self.popen.kill()
+
+  def _quit_browser(self):
+    """
+    Quits the browser.
+    """
+    _url = f'{ADDRESS}/session/{self.session_id}'
+    _res = requests.delete(_url)
+
+  def exit(self):
+    """
+    Exits the program and deallocates all running processes.
+    """
+    self._quit_browser()
+    self._kill()
+
