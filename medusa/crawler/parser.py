710603d62107734fe73da18f41eb0da47adc181a
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-05-02 09:27:58 -0400

    implemented prefix and suffix searching algorithm for DOM parser

--- medusa/crawler/parser.py
+++ medusa/crawler/parser.py
@@ -38,18 +38,39 @@ class Parser:
     _raw_text = self._response.text
 
     for i in range(0, 1):
-      _prefix_start, _prefix_end = re.search(
-        '<[a-zA-Z]*>', self._response.text
-      ).span()
+      # search for any occurence of opening tag and get start and end positions
+      _pfx_start, _pfx_end = re.search('<[a-z|A-Z|\s|=|"]*>', _raw_text).span()
 
-      _prefix = re.sub('<|>', '', _raw_text[_prefix_start:_prefix_end])
+      # extract contents of starting tag
+      _prefix = re.sub('<|>', '', _raw_text[_pfx_start:_pfx_end])
 
-      _suffix = re.search(
-        '</div>', self._response.text
-      )
+      # get first string in starting tag, should be type
+      _DOM_type = _prefix.split(' ')[0]
+
+      # remove prefix from raw text
+      _raw_text = _raw_text[_pfx_end:]
+
+      # check if element is self-closing
+      if _DOM_type in self._self_closing_html_tags:
+        # insert into tree
+        ############# inserting
+        pass
+      else:
+        # search for suffix
+        _sfx_start, _sfx_end = re.search(
+          f'</{_DOM_type}>', _raw_text
+        ).span()
+
+        _suffix = re.sub('<|>', '', _raw_text[_sfx_start:_sfx_end])
+
+        # remove suffix
+        _raw_text = _raw_text[:_sfx_start]
+
+        # insert into tree
+        ############# inserting
 
       print(
-        _prefix
+        _raw_text
       )
 
     
