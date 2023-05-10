3f99681af1f039e570af63ffcdc05d464a089a63
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-05-10 18:09:03 -0400

    cleaned up parse dom function with helper functions yet some of the functionality still remains to be implemented

--- medusa/crawler/parser.py
+++ medusa/crawler/parser.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Tuple
 import re
 from requests import Response
 from medusa.crawler.data import DOMNode
@@ -94,6 +94,24 @@ class Parser:
       parent.insert_child(_node)
     else:
       self.elements.insert_child(_node)
+
+
+  def _parse_open_tag(self, open: str) -> Tuple[str, dict]:
+    # remove carat delimeters
+    open = open[1:-1]
+
+    # sanitize potential closing delimeter for self-closing tags
+    if open[-1] == '/':
+      open = open[:-1]
+
+    # extract type and attributes
+    _type = open.split(' ', 1)[0]
+    _attributes = {}
+
+    if open.split(' ', 1) > 1:
+      _attributes = self._extract_attributes(open.split(' ', 1)[1])
+
+    return (_type, _attributes)
   
 
   def _parse_DOM(self, DOM: str, parent: Optional[DOMNode]=None) -> None:
@@ -105,67 +123,36 @@ class Parser:
         _open_i, _open_f = _open_search.span()
         
         if _open_i != 0:                          # text at start of string
-          _node = DOMNode(
+          self._create_node(
             type='text',
             attributes={
               'content': DOM[:_open_i]
             }
           )
 
-          if parent:
-            parent.insert_child(_node)
-          else:
-            self.elements.insert_child(_node)
-
-          DOM = DOM[_open_i:]
-
-          self._parse_DOM(DOM, parent)
+          # parse with remainder of DOM after text
+          self._parse_DOM(DOM[_open_i:], parent)
         else:                                     # element at start of string
-          _open = DOM[_open_i + 1:_open_f - 1]
-
-          # sanitize potential closing delimeter for self-closing tags
-          if _open[-1] == '/':
-            _open = _open[:-1]
-
-          # extract type and attributes
-          _type = _open.split(' ', 1)[0]
-
-          _attributes = {}
-
-          if _open.split(' ', 1) > 1:
-            _attributes = self._extract_attributes(_open.split(' ', 1)[1])
-
-          # remove open tag from dom
-          DOM = DOM[_open_f:]
+          _type, _attributes = self._parse_open_tag(DOM[_open_i:_open_f])
 
           if _type in self.self_closing_elements:  # element is self-closing
-            _node = DOMNode(
+            self._create_node(
               type=_type,
               attributes=_attributes
             )
 
-            if parent:
-              parent.insert_child(_node)
-            else:
-              self.elements.insert_child(_node)
-
-            # continue with remainder
-            self._parse_DOM(DOM, parent)
+            # parse with remainder of DOM after self-closing tag
+            self._parse_DOM(DOM[_open_f:], parent)
           else:                                   # element is not self-closing
             pass
       else:                                       # open tag not found
         # everything treated as text
-        _node = DOMNode(
+        self._create_node(
           type='text',
           attributes={
             'content': DOM
           }
         )
-
-        if parent:
-          parent.insert_child(_node)
-        else:
-          self.elements.insert_child(_node)
       
 
   """
