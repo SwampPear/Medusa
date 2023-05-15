a6cc0c84fc7b15a1febf731fbe5d5536304d4606
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-05-15 00:02:40 -0400

    cleaned up DOMNode class

--- medusa/crawler/data.py
+++ medusa/crawler/data.py
@@ -1,31 +1,17 @@
 class DOMNode:
-  def __init__(
-    self, 
-    type: str, 
-    attributes: dict={}
-  ) -> None:
+  def __init__(self, type: str, attributes: dict={}) -> None:
     self.type = type
-    self._attributes = attributes
-    self._children = []
+    self.attributes = attributes
+    self.children = []
 
-  
   def __str__(self) -> str:
-    _children = []
-
-    for _child in self._children:
-      _children.append(str(_child))
-
-    _output = f'Type: {self.type}; Attributes: {self._attributes}; Children: {_children}'
-
-    return _output
-  
+    children = [str(child) for child in self.children]
+    output = f'Type: {self.type}; Attributes: {self.attributes}; Children: {children}'
+    
+    return output
 
   def insert_child(self, child: 'DOMNode') -> None:
-    self._children.append(child)
-
+    self.children.append(child)
 
   def get_attribute(self, attribute: str) -> str:
-    try:
-      return self._attributes[attribute]
-    except:
-      return None
+    return self.attributes.get(attribute, None)
