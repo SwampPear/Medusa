d694e65c388d6bf9a804b2cf9da6079bf42603e4
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-05-15 00:44:28 -0400

    cleaned up app.py

--- medusa/app.py
+++ medusa/app.py
@@ -5,40 +5,39 @@ from medusa.cli import CLI, Color
 
 class App:
   def __init__(self) -> None:
-    self._browser = None
-    self._cli = CLI()
+    self.browser = None
+    self.cli = CLI()
 
   
   def _execute_command(self, input: str) -> None:
-    _args = input.split(' ')
-    _cmd = _args[0].lower()
+    args = input.split(' ')
+    cmd = args[0].lower()
 
-    if _cmd == 'exit': self._exit()
-    elif _cmd == 'clear': self._clear()
-    else: self._invalid(_cmd)
+    if cmd == 'exit':
+      self._exit()
+    elif cmd == 'clear':
+      self._clear()
+    else:
+      self._invalid(cmd)
 
 
   def _exit(self) -> None:
-    if self._browser: self._browser.exit()
+    if self.browser:
+      self.browser.exit()
 
-    self._cli.write('Medusa terminated.', Color.INFO, True)
+    self.cli.write('Medusa terminated.', Color.INFO, True)
     sys.exit(0)
 
   
   def _clear(self) -> None:
-    self._cli.clear()
+    self.cli.clear()
 
   
   def _invalid(self, command: str) -> None:
-    self._cli.write(f'Invalid command: {command}', Color.DANGER, True)
+    self.cli.write(f'Invalid command: {command}', Color.DANGER, True)
 
 
   def run(self) -> None:
     while True:
-      _input = self._cli.read(
-        '~ ',
-        Color.DELIMITER,
-        True
-      )
-      
-      self._execute_command(_input)
+      input = self.cli.read('~ ', Color.DELIMITER, True)
+      self._execute_command(input)
