a6be4bed60b3d4e2fdce9e92f27f986bdd6a3e9f
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-05-15 00:50:04 -0400

    mostly everything cleaned up and version updated

--- medusa/cli.py
+++ medusa/cli.py
@@ -12,42 +12,37 @@ class Color:
 
 class CLI:
   def __init__(self) -> None:
-    self.write(
-      '\n'.join([
+    title = [
         '    __  _____________  __  _______ ___ ',
         '   /  |/  / ____/ __ \/ / / / ___//   |',
         '  / /|_/ / __/ / / / / / / /\__ \/ /| |',
         ' / /  / / /___/ /_/ / /_/ /___/ / ___ |',
         '/_/  /_/_____/_____/\____//____/_/  |_|'
-      ]), 
-      Color.DELIMITER, 
-      True,
-      count=False
-    )
-    self.write(
-      '\n'.join([
+    ]
+    info = [
         '@swamppear',
-        'v0.0.2',
-        ''
-      ]), 
-      Color.SUCCESS, 
-      True,
-      count=False
-    )
+        'v0.0.25'
+    ]
 
-    self.row = 0
+    self.write('\n'.join(title), Color.DELIMITER, True, count=False)
+    self.write('\n'.join(info), Color.SUCCESS, True, count=False)
+    self.write(count=False)
 
+    self.row_count = 0
 
+  
   def write(
     self,
-    text: str='', 
-    color: Optional[str]=None, 
-    bold: bool=False, 
+    text: str='',
+    color: Optional[str]=None,
+    bold: bool=False,
     end: str='\n',
-    count: bool=True 
-  ) -> None:
-    if color: text = f'{color}{text}'
-    if bold: text = f'\033[1m{text}'
+    count: bool=True
+) -> None:
+    if color:
+      text = f'{color}{text}'
+    if bold:
+      text = f'\033[1m{text}'
 
     text = f'{text}\033[0m{end}'
 
@@ -55,7 +50,7 @@ class CLI:
     sys.stdout.flush()
 
     if count:
-      self.row += 1
+      self.row_count += 1
 
 
   def read(
@@ -71,14 +66,17 @@ class CLI:
     while True:
       char = ord(sys.stdin.read(1))
       
-      if char == 3: return
-      elif char in (10, 13): return text
-      else: text += chr(char)
+      if char == 3: 
+        return
+      elif char in (10, 13): 
+        return text
+      else: 
+        text += chr(char)
 
 
   def clear(self) -> None:
-    sys.stdout.write(f'\u001b[{self.row}F')
+    sys.stdout.write(f'\u001b[{self.row_count}F')
     sys.stdout.write('\u001b[0J')
     sys.stdout.flush()
 
-    self.row = 0
+    self.row_count = 0
